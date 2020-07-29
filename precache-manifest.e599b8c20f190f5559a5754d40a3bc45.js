64373e664d4440969f1e4ceced987266ce0fd042
Oscar Rodriguez <oscarrobertrodriguez@gmail.com>
2020-07-28 20:37:53 -0500

    Updates

---
+++ precache-manifest.e599b8c20f190f5559a5754d40a3bc45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ba0ab28866719008d59e6cbc7f0f39b",
+    "url": "/BinaryConverter/index.html"
+  },
+  {
+    "revision": "aeb3f89c791580cc0896",
+    "url": "/BinaryConverter/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "9fbb37e27a9be6fa6623",
+    "url": "/BinaryConverter/static/css/main.8fa351c7.chunk.css"
+  },
+  {
+    "revision": "aeb3f89c791580cc0896",
+    "url": "/BinaryConverter/static/js/2.b5ff200a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/BinaryConverter/static/js/2.b5ff200a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fbb37e27a9be6fa6623",
+    "url": "/BinaryConverter/static/js/main.292f0b51.chunk.js"
+  },
+  {
+    "revision": "527ef835561e2eaaff02",
+    "url": "/BinaryConverter/static/js/runtime-main.be6bf255.js"
+  }
+]);
